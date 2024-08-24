22ea78676963496459a6f93e5b31a6ef9f7fa2ac
maz-r <3256925+maz-r@users.noreply.github.com>
2024-08-24 11:20:10 +0100

    Initial push of Modulus Module code

--- Module Code/mudular_core/StepperControl.h
+++ Module Code/mudular_core/StepperControl.h
@@ -1,5 +1,13 @@
 /***************************************************
-
+# Required Notice: Copyright (C) 2024 Martin Randall - All Rights Reserved
+#
+# You may use, distribute and modify this code under the
+# terms of the PolyForm Noncommercial 1.0.0 license.
+#
+# You should have received a copy of the PolyForm Noncommercial 1.0.0 license with
+# this file. 
+# If not, please visit: <https://polyformproject.org/licenses/noncommercial/1.0.0>
+#
 ****************************************************/
 #define _STEPPER_CONTROL
 #define MAX_MOTORS        2
